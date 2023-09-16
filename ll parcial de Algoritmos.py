2eaa54f1c56d11ac734b61c43531a1e7e428d832
davidsalop <145225164+davidsalop@users.noreply.github.com>
2023-09-16 12:42:17 -0600

    parcial ii

---
+++ ll parcial de Algoritmos.py
@@ -0,0 +1,18 @@
+def mostrar_mensaje (prueba mensaje);
+    print(mensaje)
+    
+
+carga_suma(){
+    valor1=int(input("ingrese el primer valor:"))
+    valor2=int(input("ingrese el segundo valor:"))
+    suma=valor1+valor2
+    print("La suma de los dos valores es:"suma)
+
+
+ }
+ // programa principal
+
+
+mostrarmensaje("El progtama calcula la suma de dos valores ingresados por teclado.")
+cargasuma()
+mostrar_mensaje()         
---
+++ superficie de rectangulo.py
@@ -0,0 +1,2 @@
+def superficie_rectangulo(lado1, lado2):
+    return lado1 * lado2
